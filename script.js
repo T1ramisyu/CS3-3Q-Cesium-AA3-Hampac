eabb7839ddcf995296790ca2ccf05cebf2fcd35c
T1ramisyu <ckyhampac22@src.pshs.edu.ph>
2025-03-04 18:50:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function check(){
     let message = prompt("Enter a message:");
-    let string1 = "FR", string2 = "AI", string3 = "aI", string4 = "Ai";
 
     let element = document.getElementById('result');
 
-    if(message.indexOf(string1) != -1){
-        if(message.indexOf(string3) != -1 || message.indexOf(string4) != -1){
-            element.innerHTML = "The message " + "\"" + message + "\" " + "is fake!";
+    if(message.indexOf("FR") != -1){
+        if(message.indexOf("aI") != -1 || message.indexOf("Ai") != -1){
+            element.innerHTML = "The message " + "\"" + message + "\" " + "is tampered with!";
         }else{
             element.innerHTML = "The message " + "\"" + message + "\" " + "is legitimate!";
         }
-    }else if (message.indexOf(string2) != -1 || message.indexOf(string3) != -1 || message.indexOf(string4) != -1){
-        element.innerHTML = "The message " + "\"" + message + "\" " + "is fake!";
+    }else if (message.indexOf("AI") != -1 || message.indexOf("aI") != -1 || message.indexOf("Ai") != -1){
+        element.innerHTML = "The message " + "\"" + message + "\" " + "is tampered with!";
     }else{
         element.innerHTML = "The message " + "\"" + message + "\" " + "is not yet encoded!";
     }
